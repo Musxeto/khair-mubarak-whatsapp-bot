d1e37d80a0c849e7c54a6e61a91e6d32db9b40e9
musxeto <mustafamalikawan786@gmail.com>
2025-03-31 02:24:52 +0500

    lmfao wokred

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
 options = webdriver.FirefoxOptions()
-options.add_argument("--profile")
-options.add_argument("./whatsapp_data")
+options.add_argument("--profile=/home/mus/.mozilla/firefox/kecb9rer.default-release")
 
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
 driver.get("https://web.whatsapp.com/")
